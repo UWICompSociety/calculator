244602aad9639346244a790f23f7fb389b8370db
vwainne <vwainne@gmail.com>
2016-02-20 14:42:32 -0500

    Fixing issue #7

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ __author__ = 'javon'
 
 def start():
     choice = input("Press 1 to add\nPress 2 to find product\n")
-    if choice == 1:
+    if choice == '1':
         #accept numbers and add them here
-        num1 = input("Enter the first number\n")
-        num2 = input("Enter the second number\n")
+        num1 = int(input("Enter the first number\n"))
+        num2 = int(input("Enter the second number\n"))
         print ("Sum:"+str(num1+num2))
 
-    if choice == 2:
-    	num1 = input("Enter the first number\n")
-    	num2 = input("Enter the second number\n")
-    	print("Product: "str(num1*num2))
+    if choice == '2':
+    	num1 = int(input("Enter the first number\n"))
+    	num2 = int(input("Enter the second number\n"))
+    	print("Product: "+str(num1*num2))
 
 
 #main function in python
