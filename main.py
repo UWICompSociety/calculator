c7136ee8914616cb2f44841c4fd3001a352e325f
ricikay93 <ricikay1993@gmail.com>
2016-02-20 12:26:14 -0800

    #9

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ __author__ = 'javon'
 
 
 def start():
-    choice = input("Press 1 to add\nPress 2 to find product\nPress 3 to find result\nChoice")
+    choice = input("Press 1 to add\nPress 2 to find product\nPress 3 to find result\nPress 4 to find the exponent a**b\nChoice: ")
     if choice == 1:
         print add()
 
@@ -15,21 +15,27 @@ def start():
         print "Invalid choice"
 
 def add():
-	num1 =  input("Enter first number")
-	num2 = input("Enter second number")
+	num1 =  input("Enter first number: ")
+	num2 = input("Enter second number: ")
 	return "Sum: "+str(num1+num2)
 
 def multiply():
-    num1 =  input("Enter first number")
-    num2 = input("Enter second number")
+    num1 =  input("Enter first number: ")
+    num2 = input("Enter second number: ")
     return "Product: "+str(num1*num2)
 
 def division():
-    num1 =  input("Enter first number")
-    num2 = input("Enter second number")
+    num1 =  input("Enter first number: ")
+    num2 = input("Enter second number: ")
     if isinstance(num1, float):
         return "Result: "+str(num1/float(num2))
     return "Result: "+str(num1/num2)
+
+def nthpower():
+    num1 =  input("Enter first number")
+    num2 = input("Enter number to raise by")
+    return "Power of "+num1+"and "+num2+": "+str(num1**num2)
+    
 
 #main function in python
 if __name__ == "__main__":
