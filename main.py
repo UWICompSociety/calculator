a6dc70741c52b6db24c8c583e5fa2191ae3b75a1
ricikay93 <ricikay1993@gmail.com>
2016-02-20 11:54:39 -0800

    Trying to upload

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ __author__ = 'javon'
 
 
 def start():
-    choice = input("Press 1 to add\nPress 2 to find product\n")
+    choice = input("Press 1 to add\nPress 2 to find product\nPress 3 to find result\nChoice")
     if choice == 1:
-        #accept numbers and add them here
-        num1 = input("Enter the first number\n")
-        num2 = input("Enter the second number\n")
-        print ("Sum:"+str(num1+num2))
+        print add()
 
-    if choice == 2:
-    	num1 = input("Enter the first number\n")
-    	num2 = input("Enter the second number\n")
-    	print("Product: "str(num1*num2))
+    elif choice == 2:
+    	print multiply()
+    
+    elif choice == 3:
+        print division()
+    else:
+        print "Invalid choice"
 
+def add():
+	num1 =  input("Enter first number")
+	num2 = input("Enter second number")
+	return "Sum: "+str(num1+num2)
+
+def multiply():
+    num1 =  input("Enter first number")
+    num2 = input("Enter second number")
+    return "Product: "+str(num1*num2)
+
+def division():
+    num1 =  input("Enter first number")
+    num2 = input("Enter second number")
+    if isinstance(num1, float):
+        return "Result: "+str(num1/float(num2))
+    return "Result: "+str(num1/num2)
 
 #main function in python
 if __name__ == "__main__":
