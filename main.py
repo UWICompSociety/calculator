3058dd8ea2e80cfecd87c65cb78893b2b20c8d95
Javon Davis <javonldavis14@gmail.com>
2016-02-20 12:52:42 -0500

    added main python file

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'javon'
+
+
+def start():
+    choice = "Press 1 to add two numbers"
+    if choice == 1:
+        #accept numbers and add them here
+        pass
+
+
+#main function in python
+if __name__ == "__main__":
+    start()
+
