51bf57eca43b99cb944b24ec22ead22b7e42b7d5
Jhanelle Thomas <jhanelle.thomas@yahoo.com>
2016-02-20 14:27:47 -0500

    Subtraction added see Issue #4

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ __author__ = 'javon'
 
 
 def start():
-    choice = input("Press 1 to add two numbers\n")
+    choice = int(input("Press 1 to add two numbers\nPress 2 to subtract two numbers\n"))
     if choice == 1:
         #accept numbers and add them here
-        num1 = input("Enter the first number\n")
-        num2 = input("Enter the second number\n")
-        print ("Sum:"+str(num1+num2))
+        num1 = int(input("Enter the first number: \n"))
+        num2 = int(input("Enter the second number: \n"))
+        print ("Sum: "+str(num1+num2))
+    elif choice == 2:
+        #accept numbers and subtracts them here
+        num1 = int(input("Enter the first number: \n"))
+        num2 = int(input("Enter the second number: \n"))
+        print ("Difference: "+str(num1-num2))
 
 
 #main function in python
