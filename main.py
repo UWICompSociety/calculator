dd0a5a52a58ba87e932a95d47d5a8713f0907524
vwainne <vwainne@gmail.com>
2016-02-20 23:58:14 -0500

    Reupdated code structure due to conflicts which arose

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ __author__ = 'javon'
 
 
 def start():
-    # accept numbers
-    num1 = int(input("Enter the first number: \n"))
-    num2 = int(input("Enter the second number: \n"))
 
     # accept operation
     choice = input("Press 1 to add\nPress 2 to subtract two numbers\nPress 3 to find product\nPress 4 to find the exponent of a number")
     
     if choice == 1:
         # adds them here
-        print("Sum: " + str(num1 + num2))
+	num1 = int(input("Enter the first number: \n"))
+        num2 = int(input("Enter the second number: \n"))
+	print("Sum: " + str(num1 + num2))
     elif choice == 2:
         # subtracts them here
+        num1 = int(input("Enter the first number: \n"))
+	num2 = int(input("Enter the second number: \n"))
         print("Difference: " + str(num1 - num2))
     elif choice == 3:
         # multiply them here
+        num1 = int(input("Enter the first number: \n"))
+	num2 = int(input("Enter the second number: \n"))
         print("Product: " + str(num1 * num2))
     elif  choice == 4:
     	# finds the value of a number raised to an index
+        num1 = int(input("Enter the first number: \n"))
+	num2 = int(input("Enter the second number: \n"))
 	print("The result of ",num1,"raised by ",num2," = ",num1**num2)
 
 
